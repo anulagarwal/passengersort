31606a73ce2d10b27606695615feae83d85ca205
Anul Agarwal <anulagarwal12@gmail.com>
2023-02-11 17:51:50 +0530

    SDKs

---
+++ Assets/ReviewManagerGame.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google.Play.Review;
+
+public class ReviewManagerGame : MonoBehaviour
+{
+    public void RequestReview()
+    {
+        // StartCoroutine(AndroidReview());
+        var reviewManager = new ReviewManager();
+
+        // start preloading the review prompt in the background
+        var playReviewInfoAsyncOperation = reviewManager.RequestReviewFlow();
+
+        // define a callback after the preloading is done
+        playReviewInfoAsyncOperation.Completed += playReviewInfoAsync =>
+        {
+            if (playReviewInfoAsync.Error == ReviewErrorCode.NoError)
+            {
+                // display the review prompt
+                var playReviewInfo = playReviewInfoAsync.GetResult();
+                reviewManager.LaunchReviewFlow(playReviewInfo);
+            }
+            else
+            {
+                // handle error when loading review prompt
+            }
+        };
+    }
+}
